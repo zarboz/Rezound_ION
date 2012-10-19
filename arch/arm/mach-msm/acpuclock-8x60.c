9b0171ff5944883f4f2ec5b9126a17867d92171e
Chad Goodman <cgoodman@HP-EliteBook-2730p.(none)>
2012-10-19 16:02:10 -0700

    DEFCONFIG: update defconfig for linaro topology drivers

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -927,11 +927,11 @@ int processor_name_read_proc(char *page, char **start, off_t off,
 #ifdef CONFIG_CMDLINE_OPTIONS
 	if (cmdline_maxkhz) {
 		p += sprintf(p, "%u", (cmdline_maxkhz/1000));
-		p += sprintf(p, "MHz butter'ed Dual Core");
+		p += sprintf(p, "MHz BLACKOUT Dual Core");
 	} else {
 #endif
 		p += sprintf(p, "%u", (CONFIG_MSM_CPU_FREQ_MAX/1000));
-		p += sprintf(p, "MHz butter'ed Dual Core");
+		p += sprintf(p, "MHz BLACKOUT Dual Core");
 #ifdef CONFIG_CMDLINE_OPTIONS
 	}
 #endif
